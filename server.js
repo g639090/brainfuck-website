4a8d0017482d333c10f34f46cd784eed100950c3
jona <fiftynine@protonmail.com>
2016-05-13 15:01:27 +0200

    rewrite the server

--- server.js
+++ server.js
@@ -5,26 +5,22 @@ const brainfuck = require('brainfuck2000')
 
 const PORT = process.env.PORT || 3000
 
-const server = net.createServer((socket) => {
-  socket.on('data', (req) => {
+const server = net.createServer()
+server.listen(PORT)
+console.log(`opened server on ${PORT}`)
+
+server.on('connection', sock => {
+  sock.on('data', req => {
     req = req.toString()
 
     fs.readFile(path.resolve(__dirname, 'server.bf'), { encoding: 'utf8' }, (err, src) => {
       if (err) { throw err }
 
-      const program = brainfuck(src)
+      var program = brainfuck(src)
       program.run(req)
-      const res = program.resultString()
+      var res = program.resultString()
 
-      socket.end(res)
+      sock.end(res)
     })
   })
-})
-
-server.on('error', (err) => {
-  throw err
-})
-
-server.listen(PORT, () => {
-  console.log(`opened server on ${PORT}`)
 })
