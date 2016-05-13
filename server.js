c6e6d099a4f417e77501e320545efd974e6539f5
jona <fiftynine@protonmail.com>
2016-05-13 03:12:52 +0200

    i am dying

--- server.js
+++ server.js
@@ -1,27 +1,30 @@
 const net = require('net')
-const path = require('path')
 const fs = require('fs')
-const bf = require('brainfuck2000')
+const path = require('path')
+const brainfuck = require('brainfuck2000')
 
 const PORT = process.env.PORT || 3000
 
-const server = net.createServer(s => {
-  s.on('data', req => {
-    treq = req.toString()
-    fs.readFile(path.join(__dirname, 'server.bf'), {encoding: 'utf8'}, (err, src) => {
+const server = net.createServer((socket) => {
+  socket.on('data', (req) => {
+    req = req.toString()
+
+    fs.readFile(path.resolve(__dirname, 'server.bf'), { encoding: 'utf8' }, (err, src) => {
       if (err) { throw err }
-      const exec = bf(src)
-      exec.run(req)
-      const res = exec.resultString()
+
+      const program = brainfuck(src)
+      program.run(req)
+      const res = program.resultString()
+
       socket.end(res)
     })
   })
+})
 
-  s.on('error', err => {
-    throw err
-  })
+server.on('error', (err) => {
+  throw err
 })
 
 server.listen(PORT, () => {
-  console.log(`listening on ${PORT}`)
+  console.log(`opened server on ${PORT}`)
 })
